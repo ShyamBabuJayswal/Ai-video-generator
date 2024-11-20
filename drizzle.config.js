eb95422e4ee6bff91c08a98e10970ddcd7bedf87
NIhal <jaisnj541@gmail.com>
2024-11-20 21:38:45 +0530

    Database setup completed successfully with initial configuration and schema. Ready for integration

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+export default {
+    schema: "./configs/schema.js", // Ensure this path is correct
+    dialect: 'postgresql', // Ensure the ORM supports this case
+    dbCredentials: {
+        url: 'postgresql://neondb_owner:FzC7O1pURIZy@ep-fragrant-queen-a5bdf0s0.us-east-2.aws.neon.tech/ai%20video%20gen?sslmode=require'
+    }
+};
