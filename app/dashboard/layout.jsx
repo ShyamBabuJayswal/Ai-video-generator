4e05606555619b91021b29590c402e4d2c985313
NIhal <jaisnj541@gmail.com>
2024-12-01 20:35:51 +0530

    Header Completed

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './_component/Header'
+
+function DashboardLayout({children}) {
+  return (
+    <div>
+     <div>
+        <Header/>
+        {children}
+     </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
