54ed315fcfa42e133282e71fc5606fe06d299c97
NIhal <jaisnj541@gmail.com>
2024-12-01 22:48:15 +0530

    Add Dashboard with Empty State and Create Button

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+import EmptyState from './_component/EmptyState';
 
 function Dashboard() {
+    const [videoList,setVideoList] = useState([]);
   return (
-    <div>Dashboard</div>
+    <div>
+        <div className='flex justify-between items-center'>
+        <h2 className='font-bold text-2xl text-primary'>Dashboard</h2>
+        <Button>+ Create One</Button>
+        </div>
+        {/*Empty State*/}
+
+        
+            {videoList?.length==0 && <div>
+            <EmptyState/>
+        </div>
+        }
+    </div>
   )
 }
 
