0fc16cb9f86e28b5eeea8f5b7bac718055026dc2
NIhal <jaisnj541@gmail.com>
2024-12-02 14:08:47 +0530

    EmptyState Component Complete

--- app/dashboard/_component/EmptyState.jsx
+++ app/dashboard/_component/EmptyState.jsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function EmptyState() {
   return (
-    <div>EmptyState</div>
+    <div className='p-5 py-24 flex items-center flex-col mt-10 border-2 border-dashed'>
+       <h2>You do not have any short video created</h2>
+       <Button>Create New Short Video</Button>
+    </div>
   )
 }
 
